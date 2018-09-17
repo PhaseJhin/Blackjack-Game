3d2b3a2eecb5ea4e852ebb19d778f4769dde2e12
zi Jin <jinzzz@dhcp-148-62.cu-wireless-students.carleton.ca>
2018-09-17 14:03:53 -0400

    Balckjack Games Java

--- src/main/java/core/Balckjack.java
+++ src/main/java/core/Balckjack.java
@@ -1,34 +1,47 @@
 package core;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 
 
 public class Balckjack {
 	
-	private int[] card = new int[52];
+	private ArrayList<Integer> card = new ArrayList<Integer>(52);
 	private String[] suit = {"S","C","D","H"};
 	private String[] rank = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 	private ArrayList<Integer> handcard = new ArrayList<Integer>();
+	private ArrayList<Integer> Dhandcard = new ArrayList<Integer>();
 	private int point;
-	private int dealerpoint = 0;
+	private int dealerpoint;
 	private int cardNum;
 	
+
+	public void ccard() {
+		for(int x = 0; x<52;x++) {
+			card.add(x);
+		}
+	}
 	public void play() {
+		System.out.println("Welcome to Balckjack");
+		ccard();
 		shuffle();
-		dealt();
+		playerdealt();
+		dealercard();
 		morecard();
 		showhandcards();
-		dealercard();
+		outresult();
 		result();
 	}
 	private void shuffle() {
-		for(int x = 0; x<52;x++) {
+		
+		Collections.shuffle(card);
+		/*for(int x = 0; x<52;x++) {
 			card[x]=x;
 		}
 		
-		for(int y=0;y<100;y++) {
+		for(int y=0;y<26;y++) {
 			Random i = new Random();
 			
 			int x1 = Math.abs(i.nextInt()%52);
@@ -40,16 +53,28 @@ public class Balckjack {
 			
 			
 		}
+		*/
 	}
-	private void dealt() {
-		handcard.add(card[0]);
-		handcard.add(card[1]);
-		point += countpoint(card[0]);
-		point += countpoint(card[1]);
+	private void playerdealt() {
+		handcard.add(card.get(0));
+		card.remove(0);
+		handcard.add(card.get(0));
+		card.remove(0);
+		point += countpoint(handcard.get(0));
+		point += countpoint(handcard.get(1));
 		cardNum = 2;
+
+		System.out.print("\nPlayer hand card :");
+		for (Integer in:handcard) {
+			System.out.print(suit[in/13]+""+rank[in%13]+"\t");
+		}
+		System.out.println(" \nCurentPoint :" + point);
 	}
 	private int countpoint(int i) {
 		int j = 0;
+		if(i%13 ==1) {
+			j=11;
+		}
 		if (i%13+1>10) {
 			j =10;
 		}else {
@@ -66,12 +91,13 @@ public class Balckjack {
 				break;
 			}
 			
-			System.out.println("Hit? (iput N to stand)");
+			System.out.println("\nHit? (iput N to stand and input anything else to Hit)");
 			if (new Scanner(System.in).next().equalsIgnoreCase("N")) {
 				break;
 			}else {
-				handcard.add(card[cardNum]);
-				point += countpoint(card[cardNum]);
+				handcard.add(card.get(0));
+				card.remove(0);
+				point += countpoint(card.get(handcard.size()-1));
 				cardNum++;
 			}
 			
@@ -80,34 +106,62 @@ public class Balckjack {
 	}
 	
 	private void showhandcards() {
+		System.out.print("\nPlayer hand card :");
 		for (Integer in:handcard) {
-			System.out.print(suit[in/13]+" "+rank[in%13]+"\t");
+			System.out.print(suit[in/13]+""+rank[in%13]+"\t");
 		}
-		System.out.println("CurentPoint" + point);
+		System.out.println(" \nCurentPoint :" + point);
 	}
 	
 	private void dealercard() {
-		Random i = new Random();
-		dealerpoint = Math.abs(i.nextInt())%10+16;
-		System.out.println("\n Dealerpoint:"+dealerpoint);
+		Dhandcard.add(card.get(0));
+		card.remove(0);
+		Dhandcard.add(card.get(0));
+		card.remove(0);
+		dealerpoint += countpoint(Dhandcard.get(0));
+		dealerpoint += countpoint(Dhandcard.get(1));
+		System.out.print(" \nDealer First Card :");
+		System.out.print(suit[Dhandcard.get(0)/13]+""+rank[(Dhandcard.get(0)%13)]+"\t");
+		//Random i = new Random();
+		//dealerpoint = Math.abs(i.nextInt())%10+16;
 	}
 	
 	private void result() {
 		if(point > 21) {
-			System.out.println("Player lost");
+			System.out.println("\nPlayer lost");
 		}else if(point == dealerpoint) {
-			System.out.println("Player lost");
+			System.out.println("\nPlayer lost");
 		}else if(dealerpoint > 21) {
-			System.out.println("Player Win");
+			System.out.println("\nPlayer Win");
 		}else if(point > dealerpoint) {
-			System.out.println("Player Win");
+			System.out.println("\nPlayer Win");
 		}else {
-			System.out.println("Player lost");
+			System.out.println("\nPlayer lost");
 		}
+	}
+	private void outresult() {
+		System.out.print("\nDealer hand card :");
+		for(int i=0; i< Dhandcard.size() ; i++) {
+				System.out.print(suit[Dhandcard.get(i)/13]+""+rank[Dhandcard.get(i)%13]+"\t");
+		}
+		System.out.println("\nDealerpoint:"+dealerpoint);
 	}
 	
 	public static void main(String[] args) {
-		new Balckjack();
+		boolean playagain = true;
+		while (playagain == true) {
+			Balckjack game = new Balckjack();
+			game.play();
+			System.out.println("\nDo you want to play again ? (Y/N)");
+			if (new Scanner(System.in).next().equalsIgnoreCase("N")) {
+				playagain = false;
+			} 
+			else{
+				playagain = true;
+			}
+			
+		}
+		
 	}
 	
 }
